8a15df2789ad1f5d274536010e1550334a6b85fb
Logan Pile <logan.pile@gmail.com>
2019-10-27 18:24:42 -0600

    Add home page register link spec

--- spec/features/home/index_spec.rb
+++ spec/features/home/index_spec.rb
@@ -2,11 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Home Index', type: :feature do
   describe 'as a visitor' do
+    before :each do
+      visit root_path
+    end
+
     it 'loads the page' do
-      visit '/'
-      
       expect(current_path).to eq(root_path)
       expect(status_code).to eq(200)
+    end
+
+    it 'has link to register' do
+      expect(page).to have_link('Register')
+      click_link 'Register'
+      expect(current_path).to eq(new_user_registration_path)
     end
   end
 end
